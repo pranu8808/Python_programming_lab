b27c135a927a95448dbf55e80152ff1175c1e3f3
pranu8808 <77329024+pranu8808@users.noreply.github.com>
2022-05-26 01:29:26 +0530

    Create even&odd.py

---
+++ even&odd.py
@@ -0,0 +1,13 @@
+list = []
+num = int(input("How many numbers:"))
+for n in range(num):
+    number= int(input("Enter the number:"))
+    list.append(number)
+even=0
+odd = 0
+for i in range(num):
+    if (list[i]%2==0):
+        even = even + list[i]
+    else:
+        odd = odd + list[i]
+print("Sum of even:",even)
